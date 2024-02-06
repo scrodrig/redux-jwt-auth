3962fa687270aa1e64856691120f6e14cd6b883c
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-06 21:34:33 +0000

    Refactor routes and add authentication feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+import Layout from './components/Layout'
+import Login from './features/auth/Login'
+import Public from './components/Public'
+import RequireAuth from './features/auth/RequireAuth'
+import Welcome from './features/auth/Welcome'
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                {/* public routes */}
+                <Route index element={<Public />} />
+                <Route path="login" element={<Login />} />
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                {/* protected routes */}
+                <Route element={<RequireAuth />}>
+                    <Route path="welcome" element={<Welcome />} />
+                </Route>
+            </Route>
+        </Routes>
+    )
 }
 
 export default App
