c594314bb2aea6dd020fe799bd6b35aca939c9bd
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-06 21:28:13 +0000

    Add RequireAuth component for authentication

---
+++ src/features/auth/RequireAuth.jsx
@@ -0,0 +1,12 @@
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
+
+import { selectCurrentToken } from './authSlice'
+import { useSelector } from 'react-redux'
+
+const RequireAuth = () => {
+    const token = useSelector(selectCurrentToken)
+    const location = useLocation()
+
+    return token ? <Outlet /> : <Navigate to="/login" state={{ from: location }} replace />
+}
+export default RequireAuth
