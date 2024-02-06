add847d1b7bb90bcf281529288b7aa1ebb82bf67
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-06 21:28:02 +0000

    Add store configuration

---
+++ src/app/store.js
@@ -0,0 +1,12 @@
+import { apiSlice } from './api/apiSlice'
+import authReducer from '../features/auth/authSlice'
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        auth: authReducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
+})
